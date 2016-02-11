13046e91f0f5433c3886fca15715ddb1d34b8eaa
lizandro <lizandro23difo@gmail.com>
2016-02-11 14:40:50 -0400

    Contacto update y show funcionando

--- app/controllers/contactos_controller.rb
+++ app/controllers/contactos_controller.rb
@@ -1,9 +1,48 @@
 class ContactosController < ApplicationController
 
+ def index
+
+  end
+
+
+
+  def edit
+      @contacto = Contacto.find(params[:id])
+      @enc_contacto = EncContacto.find(params[:enc_contacto_id])
+  end
+
+
+    def update
+        @contacto = Contacto.find(params[:id])
+      @enc_contacto = EncContacto.find(params[:enc_contacto_id])
+        respond_to do |format|
+          if @contacto.update(contacto_params)
+              format.html { redirect_to @enc_contacto, notice: 'Contacto was successfully updated.' }
+              format.json { render :show, status: :ok, location: @enc_contacto }
+          else
+            format.html { render :edit }
+            format.json { render json: @contacto.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+
+
     def create
         @enc_contacto = EncContacto.find(params[:enc_contacto_id])
         @contacto = @enc_contacto.contactos.create(contacto_params)
         redirect_to enc_contacto_path(@enc_contacto)
+  end
+
+    def destroy
+
+    @contacto = Contacto.find(params[:id])
+        @enc_contacto = EncContacto.find(params[:enc_contacto_id])
+    @contacto.destroy
+    respond_to do |format|
+        format.html { redirect_to enc_contacto_path(@enc_contacto), notice: 'Contacto was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
